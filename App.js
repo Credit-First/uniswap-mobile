bdbf2a35829800789ef261bae1e20ff9ab3dc6f8
swlaunch <peridot.lib15@gmail.com>
2020-04-26 09:46:55 -0600

    Add tab navigation

--- App.js
+++ App.js
@@ -7,8 +7,15 @@
  */
 
 import React, { useEffect } from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
 import PINCode, { hasUserSetPinCode } from '@haskkor/react-native-pincode';
+import { AccountsScreen } from './app/screens';
 
 const App = () => {
   useEffect(() => {
@@ -24,13 +31,39 @@ const App = () => {
     }
   };
 
+  const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let icon;
+      if (route.name === 'Accounts') {
+        icon = require('./assets/icons/accounts.png');
+      } else if (route.name === 'Transfer') {
+        icon = require('./assets/icons/transfer.png');
+      } else if (route.name === 'Vote') {
+        icon = require('./assets/icons/vote.png');
+      } else if (route.name === 'Transactions') {
+        icon = require('./assets/icons/transactions.png');
+      } else if (route.name === 'Convert') {
+        icon = require('./assets/icons/coins.png');
+      }
+
+      return <Image source={icon} style={{ tintColor: color }} />;
+    },
+  });
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{ flex: 1 }}>
-        <PINCode status={'locked'} />
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={screenOptions}
+        tabBarOptions={{
+          showLabel: false,
+        }}>
+        <Tab.Screen name={'Accounts'} component={AccountsScreen} />
+        <Tab.Screen name={'Transfer'} component={AccountsScreen} />
+        <Tab.Screen name={'Vote'} component={AccountsScreen} />
+        <Tab.Screen name={'Transactions'} component={AccountsScreen} />
+        <Tab.Screen name={'Convert'} component={AccountsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ app/screens/Accounts/AccountsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AccountsScreen = props => {
+  return (
+    <View>
+    </View>
+  );
+};
+
+export default AccountsScreen;
