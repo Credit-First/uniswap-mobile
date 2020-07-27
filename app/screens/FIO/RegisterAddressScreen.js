6db735f83e90150cfd66b5eb219e2ed3af2af25d
Eugene Luzgin <eugene.luzgin@mx.com>
2020-07-27 07:22:13 -0600

    Switch FIO API to https

--- app/screens/FIO/RegisterAddressScreen.js
+++ app/screens/FIO/RegisterAddressScreen.js
@@ -27,7 +27,7 @@ const RegisterAddressScreen = props => {
   } = props;
 
   const fioEndpoint = getEndpoint('FIO');
-  const apiToken = 'YCDPh0ni7MMwrAXa1eerq3JBBFWHDjgd6RbflXVAg653Zh0';
+  const apiToken = 'XXX';
 
   const _checkAvailable = async address => {
     setAddress(address);
